d86bf322cd8bd4bfa465e64e9af15fa41412a756
entropy <bac121326@gmail.com>
2019-12-10 22:33:04 +0700

    update code

--- src/main/java/com/dimageshare/controller/UserController.java
+++ src/main/java/com/dimageshare/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.dimageshare.controller;
 
 import com.dimageshare.constant.UrlBaseApi;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping(UrlBaseApi.URL_BASE_USER)
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class UserController {
 }
