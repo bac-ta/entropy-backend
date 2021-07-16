e6683738478636d8ce537a7adad3048703efb0ad
bac-ta <bac93.it@gmail.com>
2021-07-17 01:30:21 +0700

    update

--- src/main/java/com/entropy/backend/models/rests/requests/users/UserRegistrationRequest.java
+++ src/main/java/com/entropy/backend/models/rests/requests/users/UserRegistrationRequest.java
@@ -3,42 +3,57 @@ package com.entropy.backend.models.rests.requests.users;
 import com.entropy.backend.common.constants.APIMessage;
 import com.entropy.backend.models.anonations.GenderVerifier;
 import com.entropy.backend.models.anonations.PasswordMatchingWithRepeatPassword;
+import com.entropy.backend.models.anonations.PasswordVerifier;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 /**
  * @author bac-ta
  */
-@EqualsAndHashCode(callSuper = true)
 @PasswordMatchingWithRepeatPassword.List({
         @PasswordMatchingWithRepeatPassword(first = "password", second = "repeatPassword", message = APIMessage.PASSWORD_NOT_MATCH)
 })
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserRegistrationRequest extends OpenfireUserRegistrationRequest {
+public class UserRegistrationRequest {
+    @JsonProperty(required = true)
+    @NotBlank(message = APIMessage.USER_NAME_NOT_BLANK)
+    private String username;
+
+    @NotBlank(message = APIMessage.NAME_NOT_BLANK)
+    private String name;
+
+    @Email
+    @NotBlank(message = APIMessage.EMAIL_NOT_BLANK)
+    private String email;
+
+    @NotBlank(message = APIMessage.PASSWORD_NOT_BLANK)
+    @PasswordVerifier
+    private String password;
+
     private String phone;
 
-    @JsonProperty(value = "dateOfBirth", required = true)
+    @JsonProperty(value = "dateOfBirth")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private String dateOfBirth;
 
     @NotBlank
-    @JsonProperty(value = "repeatPassword", required = true)
+    @JsonProperty(value = "repeatPassword")
     private String repeatPassword;
 
-    @JsonProperty(value = "gender", required = true)
+    @NotBlank
+    @JsonProperty(value = "gender")
     @GenderVerifier(message = APIMessage.GENDER_INVALID)
     private String gender;
 
-    @JsonProperty(value = "role", required = true)
+    @NotBlank
+    @JsonProperty(value = "role")
     private String role;
 }
